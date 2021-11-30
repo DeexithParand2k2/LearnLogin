8a7303f87c5d308f819a105e428be03e7cc0fd9a
DeexithParand2k2 <deexith2002@gmail.com>
2021-11-30 17:44:52 +0530

    renamed and title

---
+++ script/form.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyAMpPESO4ocSrzbLa-Rbw7Ok20LIq387Kk",
+authDomain: "coffeetime-f50cd.firebaseapp.com",
+databaseURL: "https://coffeetime-f50cd-default-rtdb.asia-southeast1.firebasedatabase.app",
+projectId: "coffeetime-f50cd",
+storageBucket: "coffeetime-f50cd.appspot.com",
+messagingSenderId: "1016281966972",
+appId: "1:1016281966972:web:7da8b70f50caf25ba79cf9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
